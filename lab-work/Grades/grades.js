3a5c680608808fb688b8a528e2def0f5d17fd309
emsie <mollyckanework@gmail.com>
2025-11-16 15:54:52 +0000

    grades input validation

--- lab-work/Grades/grades.js
+++ lab-work/Grades/grades.js
@@ -5,16 +5,21 @@ function getResults() {
 
     for (i = 0; i < numSubjects; i++) {
         let grade=parseInt(prompt("Please enter grade for Subject "+(i+1)+" (from 1-100%):"));
-        
-        document.getElementById("subject-results-container").innerHTML += "Subject #"+(i+1)+":  "+grade+"%"+ "<br />";
-        sum += grade;
-        
+
+        //validation
+        if (!isNaN(grade) && grade>=0 && grade<=100) {
+            document.getElementById("subject-results-container").innerHTML += "Subject #"+(i+1)+":  "+grade+"%"+ "<br />";
+
+            sum += grade;
+        } else{
+            alert("Please enter a valid grade between 0 and 100.");
+        }    
     }
 
     let avg = sum / numSubjects;
     console.log(avg);
 
-    document.getElementById("grade-avg-container").innerHTML += "Your average for " + (i + 1) + " subjects is: <br/>" + avg.toFixed(2) + "%";
+    document.getElementById("grade-avg-container").innerHTML += "Your average for " + i + " subjects is: <br/>" + avg.toFixed(2) + "%";
 
     if(avg >= 85){
         document.getElementById("grade-container").innerHTML += "A";
